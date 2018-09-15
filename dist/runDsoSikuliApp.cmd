72d898c2b0797f3908c05c9946bc5807d88bfa65
Joerg Puettner <joerg.puettner@gronau-it-cloud-computing.de>
2018-09-15 14:21:11 +0200

    Renamed and updated cmd-Files

--- dist/runDsoSikuliApp.cmd
+++ dist/runDsoSikuliApp.cmd
@@ -5,6 +5,8 @@ SET START_DIR=D:\sikuli\workspace\dso_1-sikuli-idea\dist
 cd %START_DIR%
 REM del *.jar
 
+start sikuliStandby.cmd
+
 xcopy /Y D:\sikuli\workspace\dso_1.sikuli D:\sikuli\workspace\dso_1-sikuli-idea\dist\dso_1.sikuli
 
 ECHO Baue Jar
@@ -22,7 +24,7 @@ REM ls && pwd && pause
 for %%i in (*-jar-with-dependencies.jar) DO (
   java -cp ".;./;./dso_1.sikuli" -jar %%i firstDailyRun
   
-  for /L %%k in (1, 1, 12) DO (
+  for /L %%k in (1, 1, 5) DO (
     echo #%%k Loop
     java -cp ".;./;./dso_1.sikuli" -jar %%i preventScreensaver
     sleep 120
@@ -35,5 +37,9 @@ for %%i in (*-jar-with-dependencies.jar) DO (
 
 cd %START_DIR%
 REM java -jar dso-automation-0.0.1-SNAPSHOT-jar-with-dependencies.jar secondDailyRun
+
+shutdown -a
+sleep 1
+shutdown /h /f
 
 SET START_DIR=
---
+++ dist/sikuliStandby.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+TITLE dso_1-sikuli-idea
+SET START_DIR=D:\sikuli\workspace\dso_1-sikuli-idea\dist
+
+cd %START_DIR%
+
+REM 2600s <=> 43min
+sleep 2600
+shutdown /h /f
+
+SET START_DIR=
