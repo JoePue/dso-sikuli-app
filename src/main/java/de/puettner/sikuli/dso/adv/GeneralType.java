aa8ed0e877681e817f4c90d4715e048cd8ea6f31
Joerg Puettner <joerg.puettner@gronau-it-cloud-computing.de>
2018-09-23 14:13:24 +0200

    Increased similarity

--- src/main/java/de/puettner/sikuli/dso/adv/GeneralType.java
+++ src/main/java/de/puettner/sikuli/dso/adv/GeneralType.java
@@ -6,18 +6,18 @@ import org.sikuli.script.Pattern;
 import static de.puettner.sikuli.dso.commands.ui.SikuliCommands.pattern;
 
 public enum GeneralType implements MenuButton {
-    Anslem(pattern("Anslem.png").similar(0.90)),
-    Nusala(pattern("Nusala.png").similar(0.90)),
-    Vargus(pattern("Vargus.png").similar(0.90)),
-    Dracul(pattern("Dracul.png").similar(0.90)),
-    Generalmajor(pattern("Generalmajor.png").similar(0.90)),
-    MdK(pattern("MdK.png").similar(0.90)),
-    Mary(pattern("Mary.png").similar(0.90)),
-    Veteran(pattern("Veteran.png").similar(0.90)),
-    Sensenmann(pattern("Sensenmann.png").similar(0.90)),
-    Kampfgestaehlte(pattern("Kampfgestaehlte.png").similar(0.90)),
-    Verteidigungsmeister(pattern("Verteidigungsmeister.png").similar(0.90)),
-    Taverni(pattern("TavernenGeneral.png").similar(0.90));
+    Anslem(pattern("Anslem.png").similar(0.98)),
+    Nusala(pattern("Nusala.png").similar(0.98)),
+    Vargus(pattern("Vargus.png").similar(0.98)),
+    Dracul(pattern("Dracul.png").similar(0.98)),
+    Generalmajor(pattern("Generalmajor.png").similar(0.98)),
+    MdK(pattern("MdK.png").similar(0.98)),
+    Mary(pattern("Mary.png").similar(0.98)),
+    Veteran(pattern("Veteran.png").similar(0.98)),
+    Sensenmann(pattern("Sensenmann.png").similar(0.98)),
+    Kampfgestaehlte(pattern("Kampfgestaehlte.png").similar(0.98)),
+    Verteidigungsmeister(pattern("Verteidigungsmeister.png").similar(0.98)),
+    Taverni(pattern("TavernenGeneral.png").similar(0.98));
 
     public final Pattern pattern;
 
