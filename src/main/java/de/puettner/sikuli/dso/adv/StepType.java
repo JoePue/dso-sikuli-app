b80aea885289539bee6fd5d8da3f1602e9e68451
Joerg Puettner <joerg.puettner@gronau-it-cloud-computing.de>
2018-10-14 10:48:50 +0200

    New step EXIT_DSO

--- src/main/java/de/puettner/sikuli/dso/adv/StepType.java
+++ src/main/java/de/puettner/sikuli/dso/adv/StepType.java
@@ -1,5 +1,5 @@
 package de.puettner.sikuli.dso.adv;
 
 public enum StepType {
-    ATTACK, MOVE, ALL_BACK_TO_STAR_MENU, LAND, SOLVE_QUEST, UNSET_UNITS
+    ATTACK, MOVE, ALL_BACK_TO_STAR_MENU, LAND, SOLVE_QUEST, UNSET_UNITS, EXIT_DSO
 }
