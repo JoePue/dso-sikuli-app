7e1d3c67d5edaeda39740ddeace9bd1c81509073
Joerg Puettner <joerg.puettner@gronau-it-cloud-computing.de>
2018-11-18 11:18:16 +0100

    Renamed Enum Els to Eso

--- src/main/java/de/puettner/sikuli/dso/adv/AttackUnitType.java
+++ src/main/java/de/puettner/sikuli/dso/adv/AttackUnitType.java
@@ -6,7 +6,7 @@ package de.puettner.sikuli.dso.adv;
 public enum AttackUnitType {
     Rek(70, 20), Bos(190, 20), Mil(320, 20),
     Cav(70, 75), Lnb(190, 75), Sol(320, 75),
-    Arm(70, 130), Els(190, 130), Kan(320, 130);
+    Arm(70, 130), Eso(190, 130), Kan(320, 130);
 
     final int xOffset;
     final int yOffset;
